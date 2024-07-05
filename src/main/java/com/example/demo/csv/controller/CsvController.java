310c9318bfc7a6779e65ad179142d02067bd847d
sindongha <tlsehdgk87@naver.com>
2024-07-05 18:23:05 +0900

    docs: 설명문 추가

--- src/main/java/com/example/demo/csv/controller/CsvController.java
+++ src/main/java/com/example/demo/csv/controller/CsvController.java
@@ -4,11 +4,13 @@ import com.example.demo.csv.service.CsvService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * springboot 실행 후
+ * readCsv api를 호출해줘야 DB에 Market 정보가 저장됩니다.
+ */
 
 @Slf4j
 @RequiredArgsConstructor
@@ -16,8 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CsvController {
     private final CsvService csvservice;
     @GetMapping(value = "get",produces = "application/json")
-    public void readCsv(){
+    public void readCsv() {
         csvservice.saveMarkets();
     }
-
 }
