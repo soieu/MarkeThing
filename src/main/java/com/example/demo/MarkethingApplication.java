62bcb4258f23b93c1f1f39f048155074cf8bf839
sindongha <tlsehdgk87@naver.com>
2024-07-05 15:23:03 +0900

    update: 몽고디비 어노테이션 추가

--- src/main/java/com/example/demo/MarkethingApplication.java
+++ src/main/java/com/example/demo/MarkethingApplication.java
@@ -2,8 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
+@EnableMongoAuditing //monggo db 사용.
 public class MarkethingApplication {
 
     public static void main(String[] args) {
