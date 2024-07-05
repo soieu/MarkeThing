9b4279f2696511840a994a6003ffb04657d01474
sindongha <tlsehdgk87@naver.com>
2024-07-05 17:55:20 +0900

    feat: market 삽입 기능 구현

--- src/main/java/com/example/demo/csv/dto/MarketRequestDto.java
+++ src/main/java/com/example/demo/csv/dto/MarketRequestDto.java
@@ -1,14 +1,9 @@
 package com.example.demo.csv.dto;
 
 
-import com.example.demo.chat.entiity.ChatRoom;
-import com.example.demo.entity.Market;
-import com.example.demo.marketpurchaserequest.entity.MarketPurchaseRequest;
-import com.example.demo.siteuser.entity.SiteUser;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 @Builder
 @Getter
