d1b1cda1467704170629b38b36c36b1d52f7eb87
sindongha <tlsehdgk87@naver.com>
2024-07-05 18:23:49 +0900

    fix: 파일경로 지정

--- src/main/java/com/example/demo/csv/service/CsvService.java
+++ src/main/java/com/example/demo/csv/service/CsvService.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,26 +25,27 @@ public class CsvService {
     private final MarketRepository marketRepository;
 
     @Transactional
-    public void saveMarkets(){
-        try{
-            BufferedReader br = Files.newBufferedReader(Paths.get("/Users/sindongha/Desktop/market.csv"), Charset.forName("EUC-KR"));
+    public void saveMarkets() {
+        try {
+            Path fileName = Path.of("src/main/resources/template/market.csv");
+            BufferedReader br = Files.newBufferedReader(fileName, Charset.forName("EUC-KR"));
             String line = "";
 
-            while((line = br.readLine()) != null){
-                List<String> stringList = new ArrayList<String>();
+            while((line = br.readLine()) != null) {
+                List<String> stringList = new ArrayList<>();
                 String stringArray[] = line.split(",");
                 stringList = Arrays.asList(stringArray);
 
-                if(stringList.get(0).equals("식별번호")) continue; // 첫줄은 제외를 해야 함.
-                if(stringList.size()!=7)continue; // 위도, 경도가 완전하지 않은 시장은 무시.
+                if(stringList.get(0).equals("식별번호")) {
+                    continue; // 첫줄은 제외를 해야 함.
+                }
+                if(stringList.size() != 7) {
+                    continue;
+                }
 
                 int type = 0; // 상설장:0 5일장:1
-                if(stringList.get(2).equals("5일장")){
+                if(stringList.get(2).equals("5일장")) {
                     type = 1;
-                }
-                for (String s : stringList) {
-                    System.out.println(s);
-
                 }
 
                 GeometryFactory gf = new GeometryFactory(); // 포인트 객체를 처리하기 위한 클래스
@@ -56,7 +58,7 @@ public class CsvService {
                         .marketName(stringList.get(1))
                         .roadAddress(stringList.get(3))
                         .streetAddress(stringList.get(4))
-                        .point(gf.createPoint(new Coordinate(lat, lot)))
+                        .location(gf.createPoint(new Coordinate(lat, lot)))
                         .build();
                 // Market 데이터 생성
                 marketRepository.save(temp);
@@ -64,11 +66,5 @@ public class CsvService {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
-
-
 }
