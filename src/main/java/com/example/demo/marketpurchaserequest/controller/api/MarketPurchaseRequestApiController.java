816a14164904b569c2c182f4ce370dc7863b290c
sangeun-b <baek0007@algonquinlive.com>
2024-07-08 16:08:14 +0900

    refactor: 시장 의뢰글 controller return 타입 변경
    Resolves:#10

--- src/main/java/com/example/demo/marketpurchaserequest/controller/api/MarketPurchaseRequestApiController.java
+++ src/main/java/com/example/demo/marketpurchaserequest/controller/api/MarketPurchaseRequestApiController.java
@@ -3,7 +3,6 @@ package com.example.demo.marketpurchaserequest.controller.api;
 import com.example.demo.marketpurchaserequest.dto.MarketPurchaseRequestDto;
 import com.example.demo.marketpurchaserequest.service.MarketPurchaseRequestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,14 +18,12 @@ public class MarketPurchaseRequestApiController {
     private final MarketPurchaseRequestService marketPurchaseRequestService;
 
     @PostMapping("/requests")
-    public ResponseEntity createMarketPurchaseRequest(@RequestBody MarketPurchaseRequestDto request) {
+    public void createMarketPurchaseRequest(@RequestBody MarketPurchaseRequestDto request) {
         marketPurchaseRequestService.createMarketPurchaseRequest(request);
-        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/requests/{requestId}")
-    public ResponseEntity deleteMarketPurchaseRequest(@PathVariable Long requestId) {
+    public void deleteMarketPurchaseRequest(@PathVariable Long requestId) {
         marketPurchaseRequestService.deleteMarketPurchaseRequest(requestId);
-        return ResponseEntity.noContent().build();
     }
 }
