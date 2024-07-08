1adedfc795fd831cf523dd67d29dbdcc6eb10c4f
sindongha <tlsehdgk87@naver.com>
2024-07-08 18:22:07 +0900

    correct: 주석 삭제

--- src/main/java/com/example/demo/chat/controller/api/ChatMessageApiController.java
+++ src/main/java/com/example/demo/chat/controller/api/ChatMessageApiController.java
@@ -3,7 +3,6 @@ package com.example.demo.chat.controller.api;
 
 import com.example.demo.chat.dto.ChatMessageRequestDto;
 import com.example.demo.chat.service.ChatMessageService;
-import com.example.demo.chat.service.impl.ChatRoomServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
@@ -27,23 +26,6 @@ public class ChatMessageApiController {
         headerAccessor.getSessionAttributes().put("chatRoomId", requestDto.getChatRoomId());
         chatMessageService.sendMessage(requestDto);
 
-        //그러면 이제 서비스에서 채팅을 저하는 기능을 해야겠네요?
-        //chat은 화면에 표시가 되는 것이고 디비에 저장하는 것은 따로 만들어 버리기.
         template.convertAndSend("/sub/chat/room/" + requestDto.getChatRoomId(), requestDto); // 이제 보내지는 것이겠죠?
     }
-
-//    @MessageMapping("/send-message") // 채팅방에 입장을 하게 되면 생성 --> 메시지를 전송을 하게 됨.
-//    public void sendMessage(@Payload ChatDto chat, SimpMessageHeaderAccessor headerAccessor) {
-//
-//        headerAccessor.getSessionAttributes().put("roomId", chat.getRoomId());
-//        System.out.println(chat.getMessage());
-//        //그러면 이제 서비스에서 채팅을 저하는 기능을 해야겠네요?
-//        repository.sendMessage(chat); // 될려나?
-//
-//        //chat은 화면에 표시가 되는 것이고 디비에 저장하는 것은 따로 만들어 버리기.
-//        template.convertAndSend("/sub/chat/room/" + chat.getRoomId(),chat); // 이제 보내지는 것이겠죠?
-//    }
-
-
-
 }
