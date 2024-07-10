37139bcb16355ebb1703f5004f2ad6f5d568e064
sangeun-b <baek0007@algonquinlive.com>
2024-07-10 21:49:13 +0900

    test : 내 정보 조회 테스트 구현
    Resolves: #27

--- src/main/java/com/example/demo/siteuser/dto/SiteUserResponseDto.java
+++ src/main/java/com/example/demo/siteuser/dto/SiteUserResponseDto.java
@@ -1,19 +1,33 @@
 package com.example.demo.siteuser.dto;
 
-import lombok.AllArgsConstructor;
+import com.example.demo.siteuser.entity.SiteUser;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
 public class SiteUserResponseDto {
 
     private String email;
     private String name;
     private String phoneNumber;
     private String address;
-    private int manner;
+    private int mannerScore;
     private String profileImg;
+
+    @Builder
+    public SiteUserResponseDto(String email, String name, String phoneNumber, String address, int mannerScore, String profileImg) {
+        this.email = email;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.mannerScore = mannerScore;
+        this.profileImg = profileImg;
+    }
+
+    public static SiteUserResponseDto fromEntity(SiteUser siteUser) {
+        return new SiteUserResponseDto(siteUser.getEmail(), siteUser.getName(), siteUser.getPhoneNumber(),
+                siteUser.getAddress(), siteUser.getMannerScore(), siteUser.getProfileImg());
+
+    }
 
 }
--- src/test/java/com/example/demo/siteuser/controller/SiteUserApiControllerTest.java
+++ src/test/java/com/example/demo/siteuser/controller/SiteUserApiControllerTest.java
@@ -1,17 +1,21 @@
 package com.example.demo.siteuser.controller;
 
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.demo.siteuser.controller.api.SiteUserApiController;
+import com.example.demo.siteuser.dto.SiteUserResponseDto;
 import com.example.demo.siteuser.entity.SiteUser;
 import com.example.demo.siteuser.service.SiteUserService;
 import com.example.demo.type.AuthType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
@@ -57,6 +61,7 @@ public class SiteUserApiControllerTest {
             .build();
 
     @Test
+    @DisplayName("회원 삭제 테스트")
     @WithMockUser
     void deleteSiteUser() throws Exception {
         // given
@@ -64,6 +69,19 @@ public class SiteUserApiControllerTest {
         // when & then
         mockMvc.perform(MockMvcRequestBuilders.delete("/api/users").with(csrf()))
                 .andExpect(status().isOk()).andDo(print());
+    }
+
+    @Test
+    @DisplayName("내 정보 보기 테스트")
+    @WithMockUser
+    void getMyInformation() throws Exception {
+        // given
+        given(siteUserService.getMyInformation("mockEmail@gmail.com")).willReturn(
+                SiteUserResponseDto.fromEntity(siteUser));
+        // when & then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/information")).andExpect(status().isOk()).andExpect(jsonPath("$.email").value("mockEmail@gmail.com")).andDo(print());
+
+
     }
 
 }
