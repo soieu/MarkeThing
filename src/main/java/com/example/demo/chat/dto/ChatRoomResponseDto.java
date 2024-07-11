060fc4bef43836d56cc0317c36be03d4c4947bd3
sindongha <tlsehdgk87@naver.com>
2024-07-11 14:59:18 +0900

    rename: 메소드 명 변경

--- src/main/java/com/example/demo/chat/dto/ChatRoomResponseDto.java
+++ src/main/java/com/example/demo/chat/dto/ChatRoomResponseDto.java
@@ -17,7 +17,7 @@ public class ChatRoomResponseDto {
     private String title; // 의뢰글의 제목을 저장함
     private String lastChatMessage; // 시간상 마지막 메시지를 저장해서 화면에 표시함
     private String time; // 몽고 디비에 저장이 되어 있는 시간을 화면에 표시하기 위한 필드 [오전|오후] HH:MM
-    public static ChatRoomResponseDto toDto(ChatRoom chatRoom, String lastChatMessage, String time) {
+    public static ChatRoomResponseDto fromEntity(ChatRoom chatRoom, String lastChatMessage, String time) {
         return ChatRoomResponseDto.builder()
                 .chatRoomId(chatRoom.getId())
                 .title(chatRoom.getMarketPurchaseRequest().getTitle())
