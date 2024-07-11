747d474ce7d984ff30bfbb16467b0f5901d2aad4
sindongha <tlsehdgk87@naver.com>
2024-07-11 14:58:17 +0900

    update: apiController 채팅 메시지 불러오기 추가

--- src/main/java/com/example/demo/chat/controller/api/ChatRoomApiController.java
+++ src/main/java/com/example/demo/chat/controller/api/ChatRoomApiController.java
@@ -1,8 +1,11 @@
 package com.example.demo.chat.controller.api;
 
 
+import com.example.demo.chat.dto.ChatMessageResponseDto;
 import com.example.demo.chat.dto.ChatRoomRequestDto;
+import com.example.demo.chat.service.ChatMessageService;
 import com.example.demo.chat.service.ChatRoomService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ChatRoomApiController {
     private final ChatRoomService chatRoomService;
-
-    @PostMapping("api/rooms")
+    private final ChatMessageService chatMessageService;
+    // TODO: requester, request, agent 아이디를 어떻게 전송을 해야하는 지 띵킹
+    @PostMapping("api/chat/rooms")
     public void createRoom(@RequestBody ChatRoomRequestDto chatRoomRequestDto) {
         chatRoomService.createChatRoom(chatRoomRequestDto);
     }//생성을 함.
-
-
-
-    // TODO: 헤더 값에서 userId 받아오기 아직은 임의로 지정
-    @GetMapping("/api/rooms/{userId}")
+    @GetMapping(value = "api/chat/rooms/{chatRoomId}/user/{userId}")
+    public List<ChatMessageResponseDto> getChatRoom(@PathVariable("chatRoomId") Long chatRoomId, @PathVariable("userId") Long userId){
+        List<ChatMessageResponseDto> chatMessageDtos = chatMessageService.getChatMessages(chatRoomId);
+        return chatMessageDtos;
+    }
+    // TODO: 헤더 값에서 email을 받아와서 userId를 넣어줌
+    @GetMapping("/api/chat/rooms/{userId}")
     public ResponseEntity<?> getMyChatRooms(@PathVariable("userId") Long userId) {
         return ResponseEntity.ok(chatRoomService.getMyChatRooms(userId));
     }
-
 }
