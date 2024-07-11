62f7589af79203cabf010d34e38d45cf077f0e62
soieu <102034740+soieu@users.noreply.github.com>
2024-07-11 16:22:35 +0900

    test: 내 결제내역 조회 기능 테스트 Resolves:#12

--- src/test/java/com/example/demo/payment/controller/PayApiControllerTest.java
+++ src/test/java/com/example/demo/payment/controller/PayApiControllerTest.java
@@ -1,12 +1,16 @@
-package com.example.demo.payment.controller.api;
+package com.example.demo.payment.controller;
 
 import com.example.demo.payment.dto.CancelPaymentRequestDto;
+import com.example.demo.payment.dto.PayResponseDto;
 import com.example.demo.payment.dto.PaymentCallbackRequestDto;
+import com.example.demo.payment.dto.PaymentListRequestDto;
 import com.example.demo.payment.service.PaymentService;
+import com.example.demo.type.PaymentStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.siot.IamportRestClient.response.IamportResponse;
 import com.siot.IamportRestClient.response.Payment;
 import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.eq;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,11 +18,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,7 +49,7 @@ public class PayApiControllerTest {
         PaymentCallbackRequestDto request = new PaymentCallbackRequestDto();
 
         IamportResponse<Payment> response = new IamportResponse<>();
-        Mockito.when(paymentService.paymentByCallback(Mockito.any(PaymentCallbackRequestDto.class))).thenReturn(response);
+        Mockito.when(paymentService.paymentByCallback(any(PaymentCallbackRequestDto.class))).thenReturn(response);
 
         // When
         mockMvc.perform(post("/api/payments")
@@ -56,7 +66,7 @@ public class PayApiControllerTest {
         CancelPaymentRequestDto request = new CancelPaymentRequestDto();
 
         IamportResponse<Payment> response = new IamportResponse<>();
-        Mockito.when(paymentService.cancelPayment(Mockito.eq(paymentId), Mockito.any(CancelPaymentRequestDto.class))).thenReturn(response);
+        Mockito.when(paymentService.cancelPayment(Mockito.eq(paymentId), any(CancelPaymentRequestDto.class))).thenReturn(response);
 
         // When
         mockMvc.perform(post("/api/payments/{paymentId}/cancel", paymentId)
@@ -80,16 +90,30 @@ public class PayApiControllerTest {
     }
 
     @Test
-    public void testCancelPaymentWithBadRequest() throws Exception {
+    void getPaymentList_shouldReturnListOfPayments() throws Exception {
         // Given
-        String paymentId = "testPaymentId";
-        String invalidJson = "{ invalid json }";
+        Long userId = 1L;
+        PaymentListRequestDto requestDto = new PaymentListRequestDto(userId);
 
-        // When
-        mockMvc.perform(post("/api/payments/{paymentId}/cancel", paymentId)
+        PayResponseDto dto1 = new PayResponseDto("CARD", PaymentStatus.OK, 1000, LocalDateTime.now());
+        PayResponseDto dto2 = new PayResponseDto("CASH", PaymentStatus.CANCEL, 2000, LocalDateTime.now());
+        List<PayResponseDto> expectedDtos = Arrays.asList(dto1, dto2);
+
+        given(paymentService.listPayment(requestDto)).willReturn(expectedDtos);
+
+        // When & Then
+        mockMvc.perform(post("/api/payments/list")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(invalidJson))
-                // Then
-                .andExpect(status().isBadRequest());
+                        .content(objectMapper.writeValueAsString(requestDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].payMethod").value("CARD"))
+                .andExpect(jsonPath("$[0].status").value("OK"))
+                .andExpect(jsonPath("$[0].amount").value(1000))
+                .andExpect(jsonPath("$[1].payMethod").value("CASH"))
+                .andExpect(jsonPath("$[1].status").value("CANCEL"))
+                .andExpect(jsonPath("$[1].amount").value(2000));
     }
+
+
 }
