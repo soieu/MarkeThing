170838a088b2384c626ef38935c8b4d7018293b6
sangeun-b <baek0007@algonquinlive.com>
2024-07-12 19:55:47 +0900

    feat: 회원 정보 수정 기능 구현
    Resolves: #27

--- src/main/java/com/example/demo/siteuser/controller/api/SiteUserApiController.java
+++ src/main/java/com/example/demo/siteuser/controller/api/SiteUserApiController.java
@@ -2,12 +2,16 @@ package com.example.demo.siteuser.controller.api;
 
 import com.example.demo.siteuser.dto.MannerRequestDto;
 import com.example.demo.siteuser.dto.SiteUserResponseDto;
+import com.example.demo.siteuser.dto.UpdateSiteUserRequestDto;
 import com.example.demo.siteuser.service.MannerService;
 import com.example.demo.siteuser.service.SiteUserService;
+import com.example.demo.siteuser.service.impl.SiteUserServiceImpl;
+import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,18 +25,17 @@ public class SiteUserApiController {
 
     private final SiteUserService siteUserService;
     private final MannerService mannerService;
+    private final SiteUserServiceImpl siteUserServiceImpl;
 
     @DeleteMapping()
-    public void deleteSiteUser() {
-        // TODO: 로그인 된 USER 정보 입력 받아서 삭제하는 걸로 수정 필요!
-        String email = "mockEmail@gmail.com";
+    public void deleteSiteUser(Principal principal) {
+        String email = principal.getName();
         siteUserService.deleteSiteUser(email);
     }
 
     @GetMapping("/information")
-    public ResponseEntity<SiteUserResponseDto> getMyInformation() {
-        // TODO: 로그인 된 USER 정보 입력 받아서 자신의 정보만 조회 가능으로 수정 필요!
-        String email = "mockEmail@gmail.com";
+    public ResponseEntity<SiteUserResponseDto> getMyInformation(Principal principal) {
+        String email = principal.getName();
         SiteUserResponseDto response = siteUserService.getMyInformation(email);
         return ResponseEntity.ok(response);
     }
@@ -40,6 +43,17 @@ public class SiteUserApiController {
     public void createManner(@PathVariable Long userId, @RequestBody MannerRequestDto request) {
         String email = "mockEmail@gmail.com";
         mannerService.createManner(request,email,userId);
+    }
+
+    @PatchMapping()
+    public void updateSiteUser(@RequestBody UpdateSiteUserRequestDto requestDto, Principal principal) {
+        String email = principal.getName();
+//        SiteUserResponseDto siteUserResponseDto = siteUserService.getMyInformation(email);
+//        String nickName = requestDto.getNickname()==null || requestDto.getNickname().isBlank()? siteUserResponseDto.getNickname():requestDto.getNickname();
+//        String phoneNumber = requestDto.getPhoneNumber()==null || requestDto.getPhoneNumber().isBlank()? siteUserResponseDto.getPhoneNumber():requestDto.getPhoneNumber();
+//        String address = requestDto.getAddress()==null || requestDto.getAddress().isBlank()? siteUserResponseDto.getAddress():requestDto.getAddress();
+        siteUserServiceImpl.updateSiteUser(email, requestDto);
+
     }
 
 }
--- src/main/java/com/example/demo/siteuser/dto/SiteUserResponseDto.java
+++ src/main/java/com/example/demo/siteuser/dto/SiteUserResponseDto.java
@@ -2,23 +2,27 @@ package com.example.demo.siteuser.dto;
 
 import com.example.demo.siteuser.entity.SiteUser;
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 public class SiteUserResponseDto {
 
     private String email;
     private String name;
+    private String nickname;
     private String phoneNumber;
     private String address;
     private List<String> mannerScore;
     private String profileImg;
 
     @Builder
-    public SiteUserResponseDto(String email, String name, String phoneNumber, String address, List<String> mannerScore, String profileImg) {
+    public SiteUserResponseDto(String email, String name, String nickname, String phoneNumber, String address, List<String> mannerScore, String profileImg) {
         this.email = email;
         this.name = name;
+        this.nickname = nickname;
         this.phoneNumber = phoneNumber;
         this.address = address;
         this.mannerScore = mannerScore;
@@ -26,9 +30,26 @@ public class SiteUserResponseDto {
     }
 
     public static SiteUserResponseDto fromEntity(SiteUser siteUser) {
-        return new SiteUserResponseDto(siteUser.getEmail(), siteUser.getName(), siteUser.getPhoneNumber(),
-                siteUser.getAddress(), siteUser.getMannerScore(), siteUser.getProfileImg());
-
+        return SiteUserResponseDto.builder()
+                .email(siteUser.getEmail())
+                .name(siteUser.getName())
+                .nickname(siteUser.getNickname())
+                .phoneNumber(siteUser.getPhoneNumber())
+                .address(siteUser.getAddress())
+                .mannerScore(siteUser.getMannerScore())
+                .profileImg(siteUser.getProfileImg())
+                .build();
     }
 
+    public SiteUser toEntity(SiteUserResponseDto responseDto) {
+        return SiteUser.builder()
+                .email(responseDto.getEmail())
+                .name(responseDto.getName())
+                .nickname(responseDto.getNickname())
+                .phoneNumber(responseDto.getPhoneNumber())
+                .address(responseDto.getAddress())
+                .mannerScore(responseDto.getMannerScore())
+                .profileImg(responseDto.getProfileImg())
+                .build();
+    }
 }
