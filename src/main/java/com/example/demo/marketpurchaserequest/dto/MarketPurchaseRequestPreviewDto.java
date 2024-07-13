3f0df36f71f22e48dc6c8fa3fa3e3ceea6ad9258
sindongha <tlsehdgk87@naver.com>
2024-07-14 00:26:37 +0900

    feat: 데이터 타입 수정

--- src/main/java/com/example/demo/marketpurchaserequest/dto/MarketPurchaseRequestPreviewDto.java
+++ src/main/java/com/example/demo/marketpurchaserequest/dto/MarketPurchaseRequestPreviewDto.java
@@ -1,9 +1,9 @@
 package com.example.demo.marketpurchaserequest.dto;
 
 import com.example.demo.marketpurchaserequest.entity.MarketPurchaseRequest;
-import java.time.LocalDate;
+import java.sql.Time;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,8 +19,8 @@ public class MarketPurchaseRequestPreviewDto {
     private String title;
     private String content;
     private int fee;
-    private LocalTime meetupTime;
-    private LocalDate meetupDate;
+    private Time meetupTime;
+    private Date meetupDate;
     private String meetupAddress;
     private String nickname;
     private String marketName;
