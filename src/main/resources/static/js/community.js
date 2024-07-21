5b64888b2ce7805992bf9fed943dd568ac2a66b8
soieu <102034740+soieu@users.noreply.github.com>
2024-07-21 21:45:46 +0900

    feat: 게시판 대댓글 보기, 대댓글 등록 화면 추가

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,14 +1,34 @@
 let index = {
     init: function() {
-        const pathname = window.location.pathname
-        const communityId = pathname.replace('/communities/', '')
+        const pathname = window.location.pathname;
+        const communityId = pathname.replace('/communities/', '');
         this.loadDetail(communityId);
+
+        $("#create-comment").on("click", () => {
+            const pathName = window.location.pathname.split('/');
+            const communityId = pathName[pathName.length - 1];
+            console.log(pathName, communityId);
+            $.ajax({
+                url: "/api/communities/" + communityId + '/comments',
+                type: 'POST',
+                dataType: 'json',
+                contentType: 'application/json',
+                data: {
+                    content: $("#comment-content").val()
+                }
+            }).done(function(response) {
+                location.reload();
+            }).fail(function(error) {
+                console.error(error);
+            });
+        });
     },
     loadDetail: function(communityId) {
         $.ajax({
             url: "/api/communities/" + communityId,
             type: 'GET',
             dataType: 'json',
+            contentType: 'application/json',
         }).done(function(response) {
             let community = response;
             let html = '<table border="1">';
@@ -18,12 +38,62 @@ let index = {
             html += '<tr><td><strong>Created At:</strong></td><td>' + community.createdAt + '</td></tr>';
             html += '<tr><td><strong>Post Image:</strong></td><td><img src="' + community.postImg + '" alt="Post Image"></td></tr>';
             html += '</table>';
+
+            html += '<table border="1">';
+            community.comments.forEach(comment => {
+                html += '<tr id="comment-' + comment.id + '">';
+                html += '<td>' + comment.userId + '</td>';
+                html += '<td>' + comment.content + '</td>';
+                html += '<td>' + comment.createdAt + '</td>';
+                html += '<td><button onclick="index.openReply(' + comment.id + ')">Reply</button></td>';
+                html += '</tr>';
+
+                if (comment.replyComments.length > 0) {
+                    comment.replyComments.forEach(reply => {
+                        html += '<tr>';
+                        html += '<td colspan="4">ㄴ ' + reply.userId + ': ' + reply.content + ' (' + reply.createdAt + ')</td>';
+                        html += '</tr>';
+                    });
+                }
+            });
+            html += '</table>';
+
             $('#community-detail').html(html);
         }).fail(function(error) {
             console.error(error);
         });
+    },
+    openReply: function(commentId) {
+        if ($("#reply-input-" + commentId).length === 0) {
+            let replyHtml = '<tr id="reply-input-' + commentId + '">';
+            replyHtml += '<td colspan="4">';
+            replyHtml += '<input type="text" id="reply-content-' + commentId + '" placeholder="댓글을 입력하세요.">';
+            replyHtml += '<button onclick="index.createReply(' + commentId + ')">Submit</button>';
+            replyHtml += '</td>';
+            replyHtml += '</tr>';
+            $("#comment-" + commentId).after(replyHtml);
+        }
+    },
+    createReply: function(commentId) {
+        const pathName = window.location.pathname.split('/');
+        const communityId = pathName[pathName.length - 1];
+        const replyContent = $("#reply-content-" + commentId).val();
+
+        $.ajax({
+            url: '/api/communities/comments/' + commentId + '/reply-comments',
+            type: 'POST',
+            dataType: 'json',
+            contentType: 'application/json',
+            data: {
+                content: replyContent
+            }
+        }).done(function(response) {
+            location.reload();
+        }).fail(function(error) {
+            console.error(error);
+        });
     }
-}
+};
 
 $(document).ready(function() {
     index.init();
